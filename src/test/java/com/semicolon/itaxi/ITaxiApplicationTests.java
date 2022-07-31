e054046bbdc7be12a31307749e89c0635cc2a788
Megapreneur <ademolamegbabi@gmail.com>
2022-07-31 01:13:54 +0100

    Initial commit

---
+++ src/test/java/com/semicolon/itaxi/ITaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.itaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
