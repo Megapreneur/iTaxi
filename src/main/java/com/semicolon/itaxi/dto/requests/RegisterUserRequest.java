bc500ceb8375e61082f0edf91cf366e48e72ac46
Megapreneur <ademolamegbabi@gmail.com>
2022-07-31 09:47:58 +0100

    another commit to this project

--- src/main/java/com/semicolon/itaxi/dto/requests/RegisterUserRequest.java
+++ src/main/java/com/semicolon/itaxi/dto/requests/RegisterUserRequest.java
@@ -1,7 +1,15 @@
 package com.semicolon.itaxi.dto.requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterUserRequest {
     private String name;
     private String email;
     private String phoneNumber;
+    private String address;
 }
--- src/main/java/com/semicolon/itaxi/dto/response/RegisterUserResponse.java
+++ src/main/java/com/semicolon/itaxi/dto/response/RegisterUserResponse.java
@@ -1,4 +1,12 @@
 package com.semicolon.itaxi.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterUserResponse {
+    private String message;
 }
