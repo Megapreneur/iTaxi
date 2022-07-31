27e8429b92ae2c57a4f763fb0c7ebb4c9079416c
Megapreneur <ademolamegbabi@gmail.com>
2022-07-31 09:43:02 +0100

    another commit to this project

--- src/main/java/com/semicolon/itaxi/data/models/User.java
+++ src/main/java/com/semicolon/itaxi/data/models/User.java
@@ -1,8 +1,27 @@
 package com.semicolon.itaxi.data.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NonNull
     private String name;
+    @NonNull
     private String emailAddress;
+    @NonNull
     private String phoneNumber;
 }
