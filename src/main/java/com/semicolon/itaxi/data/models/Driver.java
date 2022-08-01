918da476f12da393f054b7db9c32399ec3e7e1b0
Megapreneur <ademolamegbabi@gmail.com>
2022-08-01 07:51:18 +0100

    another commit to this project

--- src/main/java/com/semicolon/itaxi/data/models/Driver.java
+++ src/main/java/com/semicolon/itaxi/data/models/Driver.java
@@ -1,4 +1,30 @@
 package com.semicolon.itaxi.data.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NonNull
+    private String name;
+    @NonNull
+    private String address;
+    @NonNull
+    private String email;
+    @NonNull
+    private String phoneNumber;
+    private String carNumber;
+    private String carType;
+    private String carColour;
 }
