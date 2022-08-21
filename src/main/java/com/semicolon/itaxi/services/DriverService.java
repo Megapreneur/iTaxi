e18dc218712a16d3b698c86e4a5d034f60c1d038
Megapreneur <ademolamegbabi@gmail.com>
2022-08-21 12:04:49 +0100

    another commit to this project

--- src/main/java/com/semicolon/itaxi/services/DriverService.java
+++ src/main/java/com/semicolon/itaxi/services/DriverService.java
@@ -1,5 +1,6 @@
 package com.semicolon.itaxi.services;
 
+import com.semicolon.itaxi.dto.requests.LoginDriverRequest;
 import com.semicolon.itaxi.dto.requests.RegisterDriverRequest;
 import com.semicolon.itaxi.dto.response.DriverDto;
 import com.semicolon.itaxi.exceptions.NoDriverFoundException;
@@ -8,6 +9,8 @@ public interface DriverService{
     DriverDto register(RegisterDriverRequest request);
 
     DriverDto getDriver(String location) throws NoDriverFoundException;
+
+    DriverDto login(LoginDriverRequest request);
 
 
 }
